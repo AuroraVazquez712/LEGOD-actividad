2d4521a50d7e50049570fc0c2ffeca6394b9d710
AuroraVazquez712 <324308734@alumno.enp.unam.mx>
2026-06-04 15:11:36 -0600

    configuración de fersa

--- elimina.php
+++ elimina.php
@@ -1,4 +1,21 @@
 <?php
+    include 'config.php';
+    $mensaje="";
+    $clase_mensaje="";
+
+    if($_SERVER["REQUEST_METHOD"]== 'POST' && isset ($_POST['set_a_eliminar'])){
+        $id_set= $_POST["set_a_eliminar"];
+
+        $sql="DELETE FROM sets WHERE set_num= '$id_set'";
+        $query= mysqli_query($conexion, $sql);
+        if($query){
+            $mensaje= "La eliminacion fue correcta";
+            $clase_mensaje="mensaje-exito";
+        }else{
+            $mensaje= "La eliminacion NO fue correcta";
+            $clase_mensaje="mensaje-error";
+        }
+    }
 
 ?>
 
